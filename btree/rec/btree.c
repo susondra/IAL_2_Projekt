e9e5707d64115d074c1fd037e9561a4c75e5cb6f
susondra <123081753+susondra@users.noreply.github.com>
2024-11-11 01:18:39 +0800

    Add files via upload

--- btree/rec/btree.c
+++ btree/rec/btree.c
@@ -65,20 +65,35 @@ bool bst_search(bst_node_t *tree, char key, bst_node_content_t **value)
  */
 void bst_insert(bst_node_t **tree, char key, bst_node_content_t value)
 {
-  bst_node_t *current_node = *tree;
   if (*tree == NULL)
-  // chyba
   {
     bst_node_t *new_note = (bst_node_t *)malloc(sizeof(bst_node_t));
     if (new_note == NULL)
     {
       return;
     }
-    new_note->content = ;
+    new_note->content = value;
     new_note->key = key;
     new_note->left = NULL;
     new_note->right = NULL;
+    *tree = new_note;
     return;
+  }
+  else if ((*tree)->key > key)
+  {
+    bst_insert(&(*tree)->left, key, value);
+  }
+  else if ((*tree)->key < key)
+  {
+    bst_insert(&(*tree)->right, key, value);
+  }
+  else
+  {
+    if ((*tree)->content.value != NULL)
+    {
+      free((*tree)->content.value);
+    }
+    (*tree)->content = value;
   }
 }
 
@@ -97,6 +112,24 @@ void bst_insert(bst_node_t **tree, char key, bst_node_content_t value)
  */
 void bst_replace_by_rightmost(bst_node_t *target, bst_node_t **tree)
 {
+  if ((*tree)->right != NULL)
+  // dokud je pravější uzel, tak jdeme hloub2ji do rekurze
+  {
+    bst_replace_by_rightmost(target, &(*tree)->right);
+  }
+  else
+  {
+    if (target->content.value != NULL)
+    // uvolnění value, pokud existuje
+    {
+      free(target->content.value);
+    }
+    target->key = (*tree)->key;
+    target->content.value = (*tree)->content.value;
+    bst_node_t *tmp = *tree;
+    *tree = (*tree)->left; // prvně nahrazení levým uzlem pokud existuje
+    free(tmp);             // uvolnění bývalého nejpravějšího uzlu
+  }
 }
 
 /*
@@ -114,6 +147,53 @@ void bst_replace_by_rightmost(bst_node_t *target, bst_node_t **tree)
  */
 void bst_delete(bst_node_t **tree, char key)
 {
+  if (*tree == NULL)
+  {
+    // Uzel se zadaným klíčem neexistuje.
+    return;
+  }
+
+  if ((*tree)->key > key)
+  {
+    // Hledáme uzel v levém podstromu.
+    bst_delete(&(*tree)->left, key);
+  }
+  else if ((*tree)->key < key)
+  {
+    // Hledáme uzel v pravém podstromu.
+    bst_delete(&(*tree)->right, key);
+  }
+  else
+  {
+    // Uzel s klíčem key nalezen.
+    if ((*tree)->left == NULL)
+    {
+      // Uzel má pravý nebo žádný podstrom.
+      bst_node_t *temp = *tree;
+      *tree = (*tree)->right;
+      if (temp->content.value != NULL)
+      {
+        free(temp->content.value);
+      }
+      free(temp);
+    }
+    else if ((*tree)->right == NULL)
+    {
+      // Uzel má pouze levý podstrom.
+      bst_node_t *temp = *tree;
+      *tree = (*tree)->left;
+      if (temp->content.value != NULL)
+      {
+        free(temp->content.value);
+      }
+      free(temp);
+    }
+    else
+    {
+      // Uzel má oba podstromy.
+      bst_replace_by_rightmost(*tree, &(*tree)->left);
+    }
+  }
 }
 
 /*
@@ -127,6 +207,24 @@ void bst_delete(bst_node_t **tree, char key)
  */
 void bst_dispose(bst_node_t **tree)
 {
+  if (*tree == NULL)
+  {
+    // Strom je již prázdný.
+    return;
+  }
+
+  bst_dispose(&(*tree)->left);
+  bst_dispose(&(*tree)->right);
+
+  if ((*tree)->content.value != NULL)
+  {
+    free((*tree)->content.value);  // Uvolní dynamicky alokovanou paměť
+    (*tree)->content.value = NULL; // Nastaví ukazatel na NULL
+  }
+  (*tree)->content.type = INTEGER; // je to navic, ale chci i contebt.type nastavit na vyzchozi hodnotu
+  (*tree)->key = -1;               // je to navic, ale chci i key nastavit na vyzchozi hodnotu
+  free(*tree);
+  *tree = NULL;
 }
 
 /*
@@ -138,6 +236,16 @@ void bst_dispose(bst_node_t **tree)
  */
 void bst_preorder(bst_node_t *tree, bst_items_t *items)
 {
+  if (tree == NULL)
+  {
+    // dosel na konec
+    return;
+  }
+
+  bst_add_node_to_items(tree, items); // preorder prvne zpracuje aktualni uzel
+
+  bst_preorder(tree->left, items);  // pote zpracuje levy podstrom
+  bst_preorder(tree->right, items); // a nakonec pravy podstrom
 }
 
 /*
@@ -149,6 +257,17 @@ void bst_preorder(bst_node_t *tree, bst_items_t *items)
  */
 void bst_inorder(bst_node_t *tree, bst_items_t *items)
 {
+  if (tree == NULL)
+  {
+    // dosel na konec
+    return;
+  }
+
+  bst_inorder(tree->left, items); // inorder prvne zpracuje levy podstrom
+
+  bst_add_node_to_items(tree, items); // pote zpracuje aktualni uzel
+
+  bst_inorder(tree->right, items); // a nakonec pravy podstrom
 }
 
 /*
@@ -160,4 +279,15 @@ void bst_inorder(bst_node_t *tree, bst_items_t *items)
  */
 void bst_postorder(bst_node_t *tree, bst_items_t *items)
 {
+  if (tree == NULL)
+  {
+    // dosel na konec
+    return;
+  }
+
+  bst_postorder(tree->left, items); // postorder prvne zpracuje levy podstrom
+
+  bst_postorder(tree->right, items); // pote zpracuje pravy podstrom
+
+  bst_add_node_to_items(tree, items); // a nakonec aktualni podstrom
 }
